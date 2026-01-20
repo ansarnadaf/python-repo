d45f8744320e4fe8fa87fb8d96ff26f78893ee1b
ansarnadaf <ansarnadaf2006@gmail.com>
2026-01-20 22:19:47 +0530

    Create 20jan.py

---
+++ 20jan.py
@@ -0,0 +1,12 @@
+# A List (ordered collection)
+fruits = ["apple", "banana", "cherry"]
+fruits.append("dragonfruit")
+
+# A Dictionary (key-value pairs)
+user_profile = {
+    "username": "dev_coder",
+    "level": 5,
+    "is_active": True
+}
+
+print(f"User {user_profile['username']} likes {fruits[0]}s.")
